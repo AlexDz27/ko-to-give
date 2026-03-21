a453cdb02211e2924a2086d056189b4098873f19
Alex Dzyuba <alexeydzyuba27@gmail.com>
2026-03-21 15:24:35 +0300

    w

--- figma/lesson-5/paddings-showcase/src/App.vue
+++ figma/lesson-5/paddings-showcase/src/App.vue
@@ -1,11 +1,37 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main class="main cont">
+    <section class="anek">
+      <!-- <h2 class="anek__title">Анекдот дня</h2> -->
+      <p>— Товарищ капитан, мне на место происшествия одному выехать или с собакой?</p>
+      <p>— С собакой, сержант. Один ум – хорошо, а два – лучше.</p>
+    </section>
+  </main>
 </template>
 
-<style scoped></style>
+<style>
+  .anek {
+    display: flex;
+    flex-direction: column;
+    /* row-gap: 8px; */
+
+    /* padding: 30px 15px; */
+
+    background-color: #F5EDD6;
+  
+    max-width: 550px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .anek > * {
+    margin: 0;
+  }
+  .anek__title {
+    text-align: center;
+    margin: 0;
+    font-weight: 500;
+  }
+</style>
